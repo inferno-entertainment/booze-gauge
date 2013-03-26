b7e4d1be4764e44dd3dff47c57ba32c60106dc2b
Eric Culp <eculperic@gmail.com>
2013-03-26 19:17:55 -0400

    Add timer to Balance Test

--- BoozeGauge/src/com/inferno/boozegauge/BalTest.java
+++ BoozeGauge/src/com/inferno/boozegauge/BalTest.java
@@ -1,16 +1,52 @@
 package com.inferno.boozegauge;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
-import android.view.View;
+import android.widget.ProgressBar;
 
 public class BalTest extends Activity {
 
+	private ProgressBar progressBar;
+	private long startTime = System.currentTimeMillis();
+	private Runnable updateProgressBar;
+	private Timer timer;
+	private final int BALANCE_TIME = 10000; //millis
+	private final int MAX_PROGRESS = 512;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bal_test);
+		progressBar = (ProgressBar) findViewById(R.id.balance_progress_bar);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		startTime = System.currentTimeMillis();
+		timer = new Timer();
+		progressBar.setMax(MAX_PROGRESS);
+		progressBar.setProgress(0);
+		updateProgressBar = new Runnable() {
+			public void run() {
+				int progress = (int) ((System.currentTimeMillis() - startTime) * 512 / BALANCE_TIME);
+				progressBar.setProgress(progress);
+				if (progress >= MAX_PROGRESS) {
+					timer.cancel();
+					endTest();
+				}
+			}
+		};
+		timer.schedule(new TimerTask() {
+			public void run() {
+				runOnUiThread(updateProgressBar);
+			}
+		}, 0L, 20L);
 	}
 
 	@Override
@@ -19,9 +55,9 @@ public class BalTest extends Activity {
 		getMenuInflater().inflate(R.menu.bal_test, menu);
 		return true;
 	}
-	public void endTest(View view) {
+
+	public void endTest() {
 		setResult(0);
 		finish();
 	}
-
 }
