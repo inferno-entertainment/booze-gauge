c555d82e8380f51ef9583734001399bbc69e7ab0
Eric Culp <eculperic@gmail.com>
2013-04-04 16:16:31 -0400

    Adds comments to balance test

--- BoozeGauge/src/com/inferno/boozegauge/BalTest.java
+++ BoozeGauge/src/com/inferno/boozegauge/BalTest.java
@@ -27,7 +27,9 @@ public class BalTest extends SuperActivity implements SensorEventListener {
 	private SensorManager sm;
 	private float acceleration;
 
+	// length of time for test
 	private final static int BALANCE_TIME = 10000; //millis
+	// full progress bar level
 	private final static int MAX_PROGRESS = 512;
 	/* TODO: configure weight for sensible scores */
 	private final static float ACCELERATION_WEIGHT = 1.0f;
@@ -42,7 +44,8 @@ public class BalTest extends SuperActivity implements SensorEventListener {
 		progressBar = (ProgressBar) findViewById(R.id.balance_progress_bar);
 		startButton = (Button) findViewById(R.id.balance_start_button);
 	}
-	
+
+	// called when start button pressed
 	public void startTest(View view) {
 		startButton.setVisibility(View.GONE);
 		startTime = System.currentTimeMillis();
@@ -50,6 +53,7 @@ public class BalTest extends SuperActivity implements SensorEventListener {
 		progressBar.setMax(MAX_PROGRESS);
 		progressBar.setProgress(0);
 		score = 0;
+		// this is called by the timer, must be run on UI thread
 		updateProgressBar = new Runnable() {
 			public void run() {
 				int progress = (int) ((System.currentTimeMillis() - startTime) * 512 / BALANCE_TIME);
@@ -60,11 +64,12 @@ public class BalTest extends SuperActivity implements SensorEventListener {
 				}
 			}
 		};
+		// Timer updates score and progress bar every 20ms
 		timer.schedule(new TimerTask() {
-
 			public void run() {
 				instantaneous_score = acceleration * ACCELERATION_WEIGHT;
 				score += instantaneous_score;
+				// UI changes have no effect on this thread
 				runOnUiThread(updateProgressBar);
 			}
 		}, 0L, 20L);
@@ -74,6 +79,7 @@ public class BalTest extends SuperActivity implements SensorEventListener {
 	@Override
 	protected void onPause() {
 		super.onPause();
+		// listening to the sensor can kill battery while paused
 		sm.unregisterListener(this);
 	}
 	
@@ -83,12 +89,16 @@ public class BalTest extends SuperActivity implements SensorEventListener {
 
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Do something here?
+		// Ignore changes
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		if (event.sensor == accelerometer) {
+			// Score is based on difference of sensor vs
+			// expected direction of gravity. This
+			// penalizes both holding the device unlevel
+			// and any motion.
 			double x = event.values[0],
 			       y = event.values[1],
 			       z = SensorManager.GRAVITY_EARTH - event.values[2];
