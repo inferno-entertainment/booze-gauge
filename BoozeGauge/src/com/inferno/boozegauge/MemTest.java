b9280bbd7d097989d5cae81187b11f2b446ba52d
James <91yera@gmail.com>
2013-04-06 13:53:05 -0400

    Memory Testable
    Threads crash my activity if started. I'm looking into the problem, but
    for the time being I'm having the first button press remove the sequence
    from view. This will allow me to collect test data at the very least.

--- BoozeGauge/src/com/inferno/boozegauge/MemTest.java
+++ BoozeGauge/src/com/inferno/boozegauge/MemTest.java
@@ -2,8 +2,10 @@ package com.inferno.boozegauge;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.Random;
 //Activity to handle the memory test
 public class MemTest extends SuperActivity {
 	public TextView textSeq; //Variable to access the user sequence display
+	Handler handler;
 	private double score = 0; //Holds the user's in test score
 	private int next = 0; //Next random number in the sequence
 	private int length; // Current length of the sequence
@@ -19,13 +22,22 @@ public class MemTest extends SuperActivity {
 	private List<Integer> attempt = new ArrayList<Integer>(); //User's guess to the sequence
 	private Random sequenceGen = new Random(); //Generates the elements to the random sequence
 	private int place = 0; //For loop counter
+	private String disp;
+	//private String disp;
+	//private int ready = 0; 
 
+	/*public void hideDisplay()
+	{
+		textSeq.setText("");
+	}*/
+	
 	//Generate and display the next sequence or call end test
 	public void nextSequence()
 	{
+		String temp = "";
 		//Get access to the user sequence display
 		textSeq = (TextView) findViewById(R.id.seqDisp);
-		if (length > 3)
+		if (length > 4)
 		{
 			//Compare the sequence to the answers
 			for(int i = 0; i < length; i++)
@@ -43,7 +55,7 @@ public class MemTest extends SuperActivity {
 		//Increase the sequence length
 		length++;
 		//A sequence of length 4 and 5 has been done
-		if (length > 5) 
+		if (length > 7) 
 		{
 			//End the test
 			endTest();
@@ -58,25 +70,85 @@ public class MemTest extends SuperActivity {
 				//TO DO: remove dependencies on this system to simplify it
 				next = sequenceGen.nextInt(4) + 1;
 				//Add the number to the sequence
+				temp += Integer.toString(next);
 				sequence.add(next);
 			}
 		}
+		//String s = "wee";
+
+		
+		
+		
+		
+		
 		
 		//Display the sequence to the user in the sequence display at regular intervals
 		//TO DO: put in  separate thread to make it work
-		/*for (place = 0; place < sequence.size(); place++)
+		for (place = 0; place < sequence.size(); place++)
 		{
 			
-			textSeq.setText("0");
+			/*textSeq.setText("0");
 			android.os.SystemClock.sleep(400);
 			textSeq.setText(Integer.toString(sequence.get(place)));
-			android.os.SystemClock.sleep(800);
-		}*/
+			android.os.SystemClock.sleep(800);*/
+			
+			
+		}
+		disp = temp;
+		//((TextView) findViewById(R.id.seqDisp)).setText(disp);
+		textSeq.setText(disp);
+		
+		/*new Thread () {
+			public void run() {
+				 handler.post(new Runnable() {
+		                public void run() {
+		                	textSeq.setText("work");
+		                }
+				 });
+			    
+			          
+				//textSeq.setText("0");
+				//android.os.SystemClock.sleep(1000);
+			//	((TextView) findViewById(R.id.seqDisp)).setText(disp);
+	
+				
+				//android.os.SystemClock.sleep(1000);
+			//	MemTest.hideDisplay();
+				//textSeq.setText("0");
+			}
+		};
+		
+		
+		
+		new Thread (new Runnable() {                                    //All click handling is in a separate thread to avoid bogging down the UI thread
+			public void run() {
+				handler.post(new Runnable() {                             //We can only touch UI elements from UI thread, so we post an anonymous Runnable
+					public void run() {                             //which is executed on the UI thread in order to change button text
+						//((TextView) findViewById(R.id.seqDisp)).setText("work");
+						}
+					});
+			}
+		}).start();*/
+			
+
+			
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
 	}
 	
 	//Button1 pressed
 	public void press1(View view)
 	{
+		textSeq.setText("");
 		//Extra press
 		if (attempt.size() >= length)
 		{
@@ -94,6 +166,7 @@ public class MemTest extends SuperActivity {
 	//Button2 pressed
 	public void press2(View view)
 	{
+		textSeq.setText("");
 		if (attempt.size() >= length)
 		{
 			nextSequence();
@@ -108,6 +181,7 @@ public class MemTest extends SuperActivity {
 	//Button3 pressed
 	public void press3(View view)
 	{
+		textSeq.setText("");
 		if (attempt.size() >= length)
 		{
 			nextSequence();
@@ -122,6 +196,7 @@ public class MemTest extends SuperActivity {
 	//Button4 pressed
 	public void press4(View view)
 	{
+		textSeq.setText("");
 		if (attempt.size() >= length)
 		{
 			nextSequence();
@@ -140,7 +215,7 @@ public class MemTest extends SuperActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mem_test);
 		//Calculate the first with a length of 4 sequence to start the test
-		length = 3;
+		length = 4;
 		nextSequence(); 
 	}
 	
@@ -158,6 +233,7 @@ public class MemTest extends SuperActivity {
 
 	//Update the Score
 	public void calculateScore() {
-		Globals.score += score;
+
+		Globals.score += 100*(score/18);
 	}
 }
