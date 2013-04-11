d6012bd2e8a497508209fc7379c95b345e372943
Greg Meltzer <meltzg@rpi.edu>
2013-04-10 21:06:18 -0400

    First attempt at popupwindow

--- BoozeGauge/src/com/inferno/boozegauge/LogTest.java
+++ BoozeGauge/src/com/inferno/boozegauge/LogTest.java
@@ -1,7 +1,11 @@
 package com.inferno.boozegauge;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.widget.PopupWindow;
 
 public class LogTest extends SuperActivity {
 	private long time;
@@ -15,6 +19,17 @@ public class LogTest extends SuperActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_log_test);
+		
+		/*PopupWindow pw = new PopupWindow();
+		pw.setContentView(R.layout.popup_instructions);*/
+		LayoutInflater inflater = (LayoutInflater)
+				this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		PopupWindow pw = new PopupWindow(
+				inflater.inflate(R.layout.popup_instructions, null, false), 
+				100, 
+				100, 
+				true);
+		pw.showAtLocation(this.findViewById(R.id.logTestView), Gravity.CENTER, 0, 0);
 	}
 
 	@Override
